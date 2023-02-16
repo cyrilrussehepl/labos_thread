dad2a8525943edfcb5a6b28bee6768bbd87d6e00
Cyril Russe <cyril.russe@hotmail.com>
2023-02-16 09:45:08 +0100

    étape 3

---
+++ ex3/thread.c
@@ -0,0 +1,136 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <time.h>
+
+typedef struct{
+   char nom[20];
+   int nbSecondes;
+}DONNEE;
+
+//Variables globales
+DONNEE data[] = { "MATAGNE",15,
+                  "WILVERS",10,
+                  "WAGNER",17,
+                  "QUETTIER",8,
+                  "",0 };
+
+DONNEE Param;                  
+pthread_t tid[4];
+pthread_mutex_t mutex_donnee, mutex_compteur;
+pthread_cond_t condCompteur;
+int compteur = 0;
+
+//fonction threads
+
+void fctFinThread(void *p){
+   printf("Thread %d.%d se termine\n", getpid(), pthread_self());
+   pthread_mutex_lock(&mutex_compteur);
+   compteur--;
+   pthread_mutex_unlock(&mutex_compteur);
+   pthread_cond_signal(&condCompteur);
+}
+
+void *fctThread(void *p){
+
+   char nom[20];
+   struct timespec time;
+   time.tv_nsec = 0;
+
+   DONNEE *par = (DONNEE*)p;
+   time.tv_sec = par->nbSecondes;
+   strcpy(nom, par->nom);
+   //Fin section critique
+   pthread_mutex_unlock(&mutex_donnee);
+
+   //set fonction de fin de thread
+   pthread_cleanup_push(fctFinThread, NULL);
+   pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+
+   
+   //set mask empty
+   sigset_t mask;
+   sigemptyset(&mask);
+   sigprocmask(SIG_SETMASK, &mask, NULL);
+   printf("thread %d.%d lancé :->nom : %s\n", getpid(), pthread_self(), nom);
+   
+   //Attente
+   nanosleep(&time, NULL);
+
+   //section fin de thread
+   pthread_cleanup_pop(1);
+   pthread_exit(NULL);
+
+   return NULL;
+}
+
+
+//Gestionnaire de signaux
+void HandlerSIGINT(int sig){
+   printf("thread %d a recu le signal SIGINT\n", pthread_self());
+}
+
+//Main
+int main(){
+   printf("Thread principale demarre : %d\n", pthread_self());
+
+   //Initialisation du mutex
+   if(pthread_mutex_init(&mutex_donnee, NULL))
+      perror("Erreur d'initialisation mutex : ");
+   
+   if(pthread_mutex_init(&mutex_compteur, NULL))
+      perror("Erreur d'initialisation mutex : ");
+
+   if(pthread_cond_init(&condCompteur, NULL))
+      perror("Erreur d'initialisation de cond : ");
+
+
+   //Armement du signal SIGINT
+   struct sigaction A;
+   A.sa_handler = HandlerSIGINT;
+   sigemptyset(&A.sa_mask);
+   A.sa_flags = 0;
+
+   if(sigaction(SIGINT, &A, NULL) == -1){
+      perror("Erreur de sigaction");
+      exit(EXIT_FAILURE);
+   }
+
+   //Masque par défaut pour tout le monde SIGINT
+   sigset_t mask;
+   sigemptyset(&mask);
+   sigaddset(&mask, SIGINT);
+   sigprocmask(SIG_SETMASK, &mask, NULL);
+
+   //Création des threads
+   for(int i =0; strcmp(data[i].nom, "")!=0;i++){
+      pthread_mutex_lock(&mutex_donnee);
+      memcpy(&Param, &data[i], sizeof(DONNEE));
+      pthread_create(&tid[i], NULL, fctThread, &Param);
+      pthread_mutex_lock(&mutex_compteur);
+      compteur++;
+      pthread_mutex_unlock(&mutex_compteur);
+   }
+
+   //Attente des threads
+   pthread_mutex_lock(&mutex_compteur);
+   while(compteur>0)
+      pthread_cond_wait(&condCompteur, &mutex_compteur);
+   pthread_mutex_unlock(&mutex_compteur);
+
+   //fin prog
+   pthread_mutex_destroy(&mutex_donnee);
+   pthread_mutex_destroy(&mutex_compteur);
+   pthread_cond_destroy(&condCompteur);
+
+   printf("Fin du thread principal\n");
+   exit(EXIT_SUCCESS);
+}
+
